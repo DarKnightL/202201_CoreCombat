5d3a26c541975bb7832f904462782c9c4b41b121
SimonLiu <liusihui@outlook.com>
2022-01-23 20:51:56 +0800

    2ndVersion_PM

---
+++ Assets/MouseMovement.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseMovement : MonoBehaviour
+{
+    CameraRaycaster cameraRaycaster;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //print(cameraRaycaster.layerHit);
+    }
+}
---
+++ Assets/Utility.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Layer
+{
+    Walkable = 8,
+    Enemy = 9,
+    RaycastEndStop=-1
+}
+
+
